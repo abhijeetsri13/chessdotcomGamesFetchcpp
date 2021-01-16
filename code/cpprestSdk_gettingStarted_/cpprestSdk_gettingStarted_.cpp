c36207b946e2c48779c898af19ef5becb9e7b2b7
akshiva <abhijeet.chess1@gmail.com>
2021-01-16 19:02:23 +0530

    added exception handling for casting

--- code/cpprestSdk_gettingStarted_/cpprestSdk_gettingStarted_.cpp
+++ code/cpprestSdk_gettingStarted_/cpprestSdk_gettingStarted_.cpp
@@ -1,6 +1,7 @@
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 #include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
 #include <string.h>
 #include <iostream>
 #include <fstream>
@@ -11,6 +12,7 @@ using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
 using namespace boost::filesystem;
+using boost::bad_lexical_cast;
 int main(int argc, char* argv[])
 {
 	auto fileStream = std::make_shared<ostream>();
@@ -18,6 +20,7 @@ int main(int argc, char* argv[])
 	int endyr = 2020;
 	utility::string_t user;
 	// Open stream to output file.
+	try {
 	if (argc < 2 || argc>4) {
 		std::cout << "\nNo Extra Command Line Argument Passed Other Than Program Name \n Ussage code.exe [username] [startYear] [endYear] \n";
 		return 0;
@@ -25,12 +28,13 @@ int main(int argc, char* argv[])
 	if (argc >= 2)
 	{
 		std::cout << "\nNumber Of Arguments Passed";
-		printf("\n----Following Are The Command Line Arguments Passed----");
+		printf("\n----Following Are The Command Line Arguments Passed----\n");
 		user = utility::conversions::to_string_t(argv[1]);
-		styr = atoi(argv[2]);
-		std::cout << styr << std::endl;
-		endyr = atoi(argv[3]);
-		std::cout << endyr << std::endl;
+		std::cout << "USERNAME: " << user << std::endl;
+		styr = boost::lexical_cast<int>(argv[2]);
+		std::cout << "START YEAR: " << styr << std::endl;
+		endyr = boost::lexical_cast<int>(argv[3]);
+		std::cout << "START YEAR: " << endyr << std::endl;
 	}
 
 
@@ -77,6 +81,11 @@ int main(int argc, char* argv[])
 								// Wait for the entire response body to be written into the file.
 								.wait();
 		}
+	}
+}
+	catch (bad_lexical_cast &)
+	{
+		std::cout << "Caught an error BAD CAST!" << std::endl;
 	}
 	return 0;
 }
